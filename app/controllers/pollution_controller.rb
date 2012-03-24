db4e43b6e657224620ac388691f220310f22c80b
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2012-03-24 12:06:16 +0800

    set the pages to cache for 1 hour

--- app/controllers/pollution_controller.rb
+++ app/controllers/pollution_controller.rb
@@ -1,9 +1,11 @@
 class PollutionController < ApplicationController
   def station
+    expires_in 1.hour, :public => true
     @station = params[:station] || :Central
   end
   
   def compare
+    expires_in 1.hour, :public => true
     @metric = params[:metric].try(:to_sym) || :fsp
     @plot_lines = Reading.plot_lines(@metric)
   end
